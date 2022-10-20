905b0f4903da3e1cf41e6182a39a7c7a6332ec5b
Ryan Reeves <reeves0ryan@gmail.com>
2022-10-19 21:40:44 -0700

    More initialization
    - React router dom
    - Fontawesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Wrapper from "./components/Wrapper"
+import Container from "./components/Container";
+import "./assets/style/style.css"
+
 
 function App() {
   return (
-    
-  );
+    <>
+      <main>
+        <Wrapper >
+          <Container />
+        </Wrapper>
+      </main>
+    </>
+  )
 }
 
 export default App;
