5d7e1efa8a9bef2befec7b248b5f8df435a871ef
Ryan Reeves <reeves0ryan@gmail.com>
2022-10-20 23:31:34 -0700

    Footer added

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 
 export default function Container() {
     return (
         <>
             <NavBar />
-
-            <Footer />
+{/* 
+            <Footer /> */}
         </>
     )
 }
