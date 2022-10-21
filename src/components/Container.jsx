2ea03c6b8ef4937dab0e632ecf115148ed4cdd36
Ryan Reeves <reeves0ryan@gmail.com>
2022-10-20 23:57:16 -0700

    Navbar styling

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "./NavBar";
-// import Footer from "./Footer";
+import Footer from "./Footer";
+import Carousel from "./Carousel";
 
 export default function Container() {
     return (
         <>
             <NavBar />
-{/* 
-            <Footer /> */}
+                <Carousel />
+            <Footer />
         </>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,18 +8,19 @@ export default function NavBar() {
     <nav>
       <div className={collapsed ? "navbar-toggler" : "navbar-toggler open"} onClick={() => toggleCollapse(!collapsed)}>
         <i className="fa-solid fa-bars"></i><i className="fa-solid fa-chevron-down navbar-hover-arrow"></i>
-        <div className={!collapsed ? "nav-link-container" : "nav-link-container collapse"}>
-          <Link to='/' className='nav-button' id='about-button'>
-            <div className='about-link'>About</div>
-          </Link>
-          <Link to='/' className='nav-button' id='contact-button'>
-            <div className='contact-link'>Contact</div>
-          </Link>
-          <Link to='/' className='nav-button' id='login-button'>
-            <div className='login-link'>Login</div>
-          </Link>
-        </div>
       </div>
+      <div className={!collapsed ? "nav-link-container" : "nav-link-container collapse"}>
+        <Link to='/' className='nav-button' id='about-button'>
+          <div className='about-link'>About</div>
+        </Link>
+        <Link to='/' className='nav-button' id='repo-button'>
+          <div className='repo-link'>Repo</div>
+        </Link>
+        <Link to='/' className='nav-button' id='contact-button'>
+          <div className='contact-link'>Contact</div>
+        </Link>
+      </div>
+      
     </nav>
   );
 }
